c469bbc4c539f883001d9daa9b1ff308af286232
praveena1212 <praveena9788@gmail.com>
2021-08-10 06:44:38 +0530

    my task done

---
+++ src/org/cts/triangle/Triangle.java
@@ -0,0 +1,16 @@
+package org.cts.triangle;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i = 1;i<=6;i++) {
+	for(int j=6 ; j>=i;j--) {
+		System.out.println("*");
+		
+	}
+	System.out.println();
+}
+	}
+
+}
