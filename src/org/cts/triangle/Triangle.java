122bad1ab0f5b93fd3644058b4f42b5a5d49f8be
praveena1212 <praveena9788@gmail.com>
2021-08-14 11:22:49 +0530

    triangle is done

--- src/org/cts/triangle/Triangle.java
+++ src/org/cts/triangle/Triangle.java
@@ -4,7 +4,11 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for(int i = 1;i<=6;i++) {
+System.out.println("welcome");
+System.out.println("praveena");
+System.out.println("murali");
+		
+		for(int i = 1;i<=6;i++) {
 	for(int j=6 ; j>=i;j--) {
 		System.out.println("*");
 		
